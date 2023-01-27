44011a658bd83bfa7314f9157e82483e9f8b1ae6
Federico Urbani <urbanifederico5@gmail.com>
2023-01-27 22:04:45 +0100

    updated 27

---
+++ Tester.java
@@ -0,0 +1,11 @@
+
+public class Tester {
+
+    public static void main(String[] args) {
+        String a="I Like to code near the Lake with my friend Luke";
+        System.out.println(a.replaceAll("L[^u]ke","BLA"));
+
+
+    }
+
+}
